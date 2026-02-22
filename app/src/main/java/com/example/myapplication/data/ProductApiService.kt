ca8f5503bcdd99d34b01712dbd98f219041dc175
vaibhavgupta <vaibhavg7005@gmail.com>
2026-02-23 04:36:29 +0530

    Ut base

--- app/src/main/java/com/example/myapplication/data/ProductApiService.kt
+++ app/src/main/java/com/example/myapplication/data/ProductApiService.kt
@@ -3,7 +3,9 @@ package com.example.myapplication.data
 import retrofit2.http.GET
 
 data class ProductResponse(
-    val products: List<ProductDto>
+//    val products: List<ProductDto>
+     val products: List<String>
+
 )
 
 interface ProductApiService {
--- app/src/main/java/com/example/myapplication/presenter/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presenter/MainActivity.kt
@@ -5,12 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -21,7 +24,9 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +51,7 @@ val LocalNavController = compositionLocalOf<NavController> {
 class MainActivity : ComponentActivity() {
 
 
-    private lateinit var viewModel: ProductViewModel
+//    private lateinit var viewModel: ProductViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,10 +62,13 @@ class MainActivity : ComponentActivity() {
         // DI setup
 
         // Create ViewModel with factory
-        viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
+//        viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
 
         setContent {
             MyApplicationTheme {
+
+
+
 //                val navController = rememberNavController()
 //                CompositionLocalProvider(LocalNavController provides navController) {
 //                    NavHost(
@@ -81,12 +89,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun RedBox() {
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(modifier = Modifier.height(100.dp).width(100.dp).background(color = Color.Red))
+    }
+}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-//        Greeting("Android")
+        RedBox()
     }
 }
