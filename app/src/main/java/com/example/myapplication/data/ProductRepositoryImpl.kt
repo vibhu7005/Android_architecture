eb70555ee766bc6ba89515166e3f5ac93f4f9281
vaibhavgupta <vaibhavg7005@gmail.com>
2026-03-05 06:54:18 +0530

    Repo module

--- app/src/main/java/com/example/myapplication/data/ProductRepositoryImpl.kt
+++ app/src/main/java/com/example/myapplication/data/ProductRepositoryImpl.kt
@@ -2,12 +2,22 @@ package com.example.myapplication.data
 
 import com.example.myapplication.domain.Product
 import com.example.myapplication.domain.ProductRepository
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
-class ProductRepositoryImpl @Inject constructor(private val apiService: ProductApiService) : ProductRepository {
-    override suspend fun fetchProducts(): Result<List<Product>> =
-        runCatching { apiService.getProducts().products.map { it.toProduct() } }
+class ProductRepositoryImpl @Inject constructor(private val apiService: ProductApiService) :
+    ProductRepository {
+    override suspend fun fetchProducts(): Result<List<Product>> {
+        try {
+            val res = apiService.getProducts()
+            return Result.success(res.products.map { it.toProduct() })
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+    }
 }
