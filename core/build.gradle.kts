1925e84166c16944ea5081c631238b08d7b92137
vaibhavgupta <vaibhavg7005@gmail.com>
2026-03-07 05:55:27 +0530

    basic init

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    kotlin("kapt")
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -10,4 +11,9 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
+}
+dependencies {
+    implementation("javax.inject:javax.inject:1")
+    implementation("com.google.dagger:dagger:${libs.versions.hilt.get()}")
+    kapt("com.google.dagger:dagger-compiler:${libs.versions.hilt.get()}")
 }
